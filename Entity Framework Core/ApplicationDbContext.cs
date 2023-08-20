0a0db646cceb0ab386788b33d53eaaa4dc284fe1
AbdallaEbrahimElanoz <anaabdoa319@gmail.com>
2023-08-20 15:51:35 +0300

    Add project files.

---
+++ Entity Framework Core/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity_Framework_Core
+{
+    public class ApplicationDbContext :DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Server=DESKTOP-9SALBLK;Database=EFCore;Trusted_Connection=True");
+        }
+    }
+}
